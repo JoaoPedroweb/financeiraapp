07032377c4333b7a5408d07adb125a2e5de3ef25
JoaoPedroweb <joaopfdossanto@gmail.com>
2022-09-26 18:58:04 -0300

    atualização do modal com button e componentes

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,9 @@
 import Modal from 'react-modal'
+import incomeImg from '../../assets/income.svg'
+import outcomImg from '../../assets/outcome.svg'
 import closeImg from '../../assets/close.svg'
-import { Conteiner } from './styles';
+import { Conteiner, TransactionTypeContainer, RadioBox } from './styles';
+import { useState } from 'react';
 
 interface NewTransactionModalProps{
     isOpen: boolean;
@@ -9,6 +12,8 @@ interface NewTransactionModalProps{
 
 
 export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModalProps) {
+ const [type, setType ] = useState('deposit');
+
   return (
     <Modal 
     isOpen={isOpen} 
@@ -37,6 +42,27 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
          type="number"
          placeholder='Valor' 
          />
+         <TransactionTypeContainer>
+           <RadioBox 
+           type= "button"
+            onClick={() => { setType('deposit'); }}
+           
+           >
+            <img src={incomeImg} alt="Entrada" />
+            <span> Entrada </span>
+           </RadioBox>
+
+
+           <RadioBox
+           type="button"
+           onClick={() => { setType('withdraw'); }}
+           
+           
+           >
+            <img src={outcomImg} alt="Saída" />
+            <span> Saída </span>
+           </RadioBox>
+         </TransactionTypeContainer>
 
          <input 
         placeholder='Categoria'
